9a70d45c350ba39b47f72a800236d92106259f20
Dmitry-Dev <i6169696@gmail.com>
2023-12-09 23:36:16 +0300

    validate form

--- src/components/JournalForm.jsx
+++ src/components/JournalForm.jsx
@@ -1,16 +1,39 @@
 import {Button} from './Button.jsx';
 import styled from 'styled-components';
+import {useState} from 'react';
 
 export const JournalForm = ({addStateItem}) => {
+    const [validForm, setValidForm] = useState({
+        title: true,
+        date: true,
+        tag: true,
+        description: true
+    });
     const addJournalItem = (e) => {
         e.preventDefault();
         const formData = Object.fromEntries(new FormData(e.target));
+        if (!formData.title) {
+            setValidForm({...validForm, title: false});
+            return;
+        }
+        if (!formData.date) {
+            setValidForm({...validForm, date: false});
+            return;
+        }
+        if (!formData.tag) {
+            setValidForm({...validForm, tag: false});
+            return;
+        }
+        if (!formData.description) {
+            setValidForm({...validForm, description: false});
+            return;
+        }
         addStateItem(formData);
     };
 
     return (
         <Form onSubmit={addJournalItem}>
-            <input type="text" name={'title'}/>
+            <input type="text" name={'title'} props={validForm.title.toString()}/>
             <input type="date" name={'date'}/>
             <input type="text" name={'tag'}/>
             <textarea name="description" cols="30" rows="10"></textarea>
@@ -24,4 +47,7 @@ const Form = styled.form`
   flex-direction: column;
   gap: 30px;
   align-items: flex-start;
+  &>input{
+    //border: ${props => props.props ? '1px solid red' : '1px solid #000'};
+  }
 `;
