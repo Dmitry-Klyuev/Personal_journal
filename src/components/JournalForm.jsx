2f2388a3d96870c68a2dc8ff0d3d56c40607b7f4
Dmitry-Dev <i6169696@gmail.com>
2023-12-17 11:45:23 +0300

    popup wit validate form and required for write

--- src/components/JournalForm.jsx
+++ src/components/JournalForm.jsx
@@ -3,46 +3,64 @@ import styled from 'styled-components';
 import {useState} from 'react';
 
 export const JournalForm = ({addStateItem}) => {
+    const [error, setError] = useState(false);
     const [validForm, setValidForm] = useState({
-        title: true,
-        date: true,
-        tag: true,
-        description: true
+        title: false,
+        date: false,
+        tag: false,
+        description: false
     });
+    // let isError = false;
+    if  (error){
+        setTimeout(()=>{
+            setError(false);
+        },3000);
+    }
+    // const error = setTimeout(()=>{
+    //     if (isError){
+    //         isError=false;
+    //     }
+    //     if (!isError){
+    //         isError=true;
+    //     }
+    // }, 1000);
     const addJournalItem = (e) => {
         e.preventDefault();
         const formData = Object.fromEntries(new FormData(e.target));
-        let isError = false;
-        if (!formData.title) {
-            setValidForm({...validForm, title: false});
-            isError = true;
+
+        if (!formData.title.trim()) {
+            setValidForm({...validForm, title: true});
+            return setError(true);
         }
-        if (!formData.date) {
-            setValidForm({...validForm, date: false});
-            isError = true;
+        if (!formData.date.trim()) {
+            setValidForm({...validForm, date: true});
+            return setError(true);
         }
-        if (!formData.tag) {
-            setValidForm({...validForm, tag: false});
-            isError = true;
+        if (!formData.tag.trim()) {
+            setValidForm({...validForm, tag: true});
+            return setError(true);
         }
-        if (!formData.description) {
-            setValidForm({...validForm, description: false});
-            isError = true;
+        if (!formData.description.trim()) {
+            setValidForm({...validForm, description: true});
+            return setError(true);
         }
-        if (isError) {
-            return;
+        if (error) {
+            return ;
         }
         addStateItem(formData);
     };
 
     return (
-        <Form onSubmit={addJournalItem}>
-            <Input type="text" name={'title'} error={validForm.title}/>
-            <Input type="date" name={'date'} error={validForm.date}/>
-            <Input type="text" name={'tag'} error={validForm.tag}/>
-            <textarea name="description" cols="30" rows="10"></textarea>
-            <Button>Сохранить</Button>
-        </Form>
+        <>
+            <Form onSubmit={addJournalItem}>
+                <Input type="text" name={'title'} />
+                <Input type="date" name={'date'} />
+                <Input type="text" name={'tag'} />
+                <textarea name="description" cols="30" rows="10"></textarea>
+                <Button>Сохранить</Button>
+            </Form>
+            {error && <Error>Все поля обязательны для заполнения</Error>}
+        </>
     );
 };
 
@@ -54,5 +72,24 @@ const Form = styled.form`
 `;
 
 const Input = styled.input`
-  border: ${props => props.error ? '' : '1px solid red'};
+  //border: ${({error}) => error === 'true' ? '1px solid red' : ''};
+`;
+
+const Error = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: red;
+  padding: 5px 20px;
+  animation: error 1s ease-in-out;
+  @keyframes error {
+    0% {
+      transform: translateX(-400px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
 `;
